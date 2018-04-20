a59e7523c5ca3ee59cc3157dee1b5577acd0cce1
Psilon <pzixel@gmail.com>
2018-04-20 17:47:09 +0300

    initial commit

---
+++ Solidity.Roslyn.Test/UnitTest1.cs
@@ -0,0 +1,1 @@
+using Solidity.Roslyn; using Xunit;  [assembly: Solidity]  namespace Solidity.Roslyn.Test {     public class ContractTest     {         [Fact]         public void Foo()         {          }     } } 
