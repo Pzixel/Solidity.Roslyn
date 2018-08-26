1e2bf385edeb69c6b4aa98ffc8e1349d84f1d0e7
Psilon <pzixel@gmail.com>
2018-08-27 01:41:20 +0300

    Make attribute working

--- Solidity.Roslyn/SolidityGenerator.cs
+++ Solidity.Roslyn/SolidityGenerator.cs
@@ -1,14 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using CodeGeneration.Roslyn;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Newtonsoft.Json;
 using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
 
 namespace Solidity.Roslyn
@@ -22,24 +25,79 @@ namespace Solidity.Roslyn
 
         public Task<SyntaxList<MemberDeclarationSyntax>> GenerateAsync(TransformationContext context, IProgress<Diagnostic> progress, CancellationToken cancellationToken)
         {
-            var solidityFiles = Directory.EnumerateFiles(context.ProjectDirectory, "*.sol", SearchOption.AllDirectories).Select(File.ReadAllText);
-            var contracts = solidityFiles.SelectMany(file => Regex.Matches(file, @"^\s*contract\s+(\w+)", RegexOptions.Multiline).Cast<Match>().Select(x => x.Groups[1].Value)).ToArray();
+            var solidityFiles = Directory.EnumerateFiles(context.ProjectDirectory, "*.sol", SearchOption.AllDirectories);
 
-            var results = contracts.Select(x=>ClassDeclaration(x)
-                .AddMembers(
-                    FieldDeclaration(
-                        VariableDeclaration(
-                            PredefinedType(Token(SyntaxKind.StringKeyword)))
-                        .AddVariables(
-                            VariableDeclarator(Identifier("Abi"))
-                            .WithInitializer(
-                                EqualsValueClause(
-                                    LiteralExpression(
-                                        SyntaxKind.StringLiteralExpression,
-                                        Literal("Some abi"))))))
-                    .WithModifiers(TokenList(Token(SyntaxKind.PublicKeyword), Token(SyntaxKind.ConstKeyword)))));
+            var jsons = solidityFiles.Select(file =>
+            {
+                var process = new Process
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                        FileName = "solc",
+                        Arguments = $"--combined-json abi,bin {file}",
+                        UseShellExecute = false,
+                        RedirectStandardOutput = true,
+                        CreateNoWindow = true
+                    }
+                };
+                process.Start();
+                string output = process.StandardOutput.ReadToEnd();
+                return output;
+            });
+
+            var contracts = jsons.Select(JsonConvert.DeserializeObject<SolcOutput>).SelectMany(x => x.Contracts);
+
+
+            var results = contracts.Select(x=>
+            {
+                int separatorIndex = x.Key.LastIndexOf(':');
+                var contract = CultureInfo.InvariantCulture.TextInfo.ToTitleCase(x.Key.Substring(separatorIndex + 1));
+                var namespaceName = Path.GetFileNameWithoutExtension(x.Key.Remove(separatorIndex));
+
+                var classDeclarationSyntax = NamespaceDeclaration(IdentifierName(namespaceName))
+                    .AddMembers(
+                        ClassDeclaration(contract)
+                            .AddMembers(
+                                FieldDeclaration(
+                                        VariableDeclaration(
+                                                PredefinedType(Token(SyntaxKind.StringKeyword)))
+                                            .AddVariables(
+                                                VariableDeclarator(Identifier("Abi"))
+                                                    .WithInitializer(
+                                                        EqualsValueClause(
+                                                            LiteralExpression(
+                                                                SyntaxKind.StringLiteralExpression,
+                                                                Literal(x.Value.Abi))))))
+                                    .WithModifiers(TokenList(Token(SyntaxKind.PublicKeyword),
+                                        Token(SyntaxKind.ConstKeyword))),
+                                FieldDeclaration(
+                                        VariableDeclaration(
+                                                PredefinedType(Token(SyntaxKind.StringKeyword)))
+                                            .AddVariables(
+                                                VariableDeclarator(Identifier("Bin"))
+                                                    .WithInitializer(
+                                                        EqualsValueClause(
+                                                            LiteralExpression(
+                                                                SyntaxKind.StringLiteralExpression,
+                                                                Literal(x.Value.Bin))))))
+                                    .WithModifiers(TokenList(Token(SyntaxKind.PublicKeyword),
+                                        Token(SyntaxKind.ConstKeyword)))));
+                return classDeclarationSyntax;
+            });
 
             return Task.FromResult(List<MemberDeclarationSyntax>(results));
         }
+    }
+
+    class SolcOutput
+    {
+        public string Version { get; set; }
+        public Dictionary<string, Contract> Contracts { get; set; }
+    }
+
+    class Contract
+    {
+        public string Abi { get; set; }
+        public string Bin { get; set; }
     }
 }
