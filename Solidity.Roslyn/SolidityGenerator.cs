66f88862dcd81d1051a1e2bd8f44964ad0851e1e
Psilon <pzixel@gmail.com>
2018-09-01 21:12:43 +0300

    format

--- Solidity.Roslyn/SolidityGenerator.cs
+++ Solidity.Roslyn/SolidityGenerator.cs
@@ -23,9 +23,11 @@ namespace Solidity.Roslyn
         {
         }
 
-        public Task<SyntaxList<MemberDeclarationSyntax>> GenerateAsync(TransformationContext context, IProgress<Diagnostic> progress, CancellationToken cancellationToken)
+        public Task<SyntaxList<MemberDeclarationSyntax>> GenerateAsync(TransformationContext context,
+                                                                       IProgress<Diagnostic> progress, CancellationToken cancellationToken)
         {
-            var solidityFiles = Directory.EnumerateFiles(context.ProjectDirectory, "*.sol", SearchOption.AllDirectories);
+            var solidityFiles =
+                Directory.EnumerateFiles(context.ProjectDirectory, "*.sol", SearchOption.AllDirectories);
 
             var jsons = solidityFiles.Select(file =>
             {
@@ -48,11 +50,11 @@ namespace Solidity.Roslyn
             var contracts = jsons.Select(JsonConvert.DeserializeObject<SolcOutput>).SelectMany(x => x.Contracts);
 
 
-            var results = contracts.Select(x=>
+            var results = contracts.Select(x =>
             {
                 int separatorIndex = x.Key.LastIndexOf(':');
-                var contract = CultureInfo.InvariantCulture.TextInfo.ToTitleCase(x.Key.Substring(separatorIndex + 1));
-                var namespaceName = Path.GetFileNameWithoutExtension(x.Key.Remove(separatorIndex));
+                string contract = CultureInfo.InvariantCulture.TextInfo.ToTitleCase(x.Key.Substring(separatorIndex + 1));
+                string namespaceName = Path.GetFileNameWithoutExtension(x.Key.Remove(separatorIndex));
 
                 var classDeclarationSyntax = NamespaceDeclaration(IdentifierName(namespaceName))
                     .AddMembers(
@@ -69,7 +71,7 @@ namespace Solidity.Roslyn
                                                                 SyntaxKind.StringLiteralExpression,
                                                                 Literal(x.Value.Abi))))))
                                     .WithModifiers(TokenList(Token(SyntaxKind.PublicKeyword),
-                                        Token(SyntaxKind.ConstKeyword))),
+                                                             Token(SyntaxKind.ConstKeyword))),
                                 FieldDeclaration(
                                         VariableDeclaration(
                                                 PredefinedType(Token(SyntaxKind.StringKeyword)))
@@ -81,7 +83,7 @@ namespace Solidity.Roslyn
                                                                 SyntaxKind.StringLiteralExpression,
                                                                 Literal(x.Value.Bin))))))
                                     .WithModifiers(TokenList(Token(SyntaxKind.PublicKeyword),
-                                        Token(SyntaxKind.ConstKeyword)))));
+                                                             Token(SyntaxKind.ConstKeyword)))));
                 return classDeclarationSyntax;
             });
 
@@ -89,13 +91,13 @@ namespace Solidity.Roslyn
         }
     }
 
-    class SolcOutput
+    public class SolcOutput
     {
         public string Version { get; set; }
         public Dictionary<string, Contract> Contracts { get; set; }
     }
 
-    class Contract
+    public class Contract
     {
         public string Abi { get; set; }
         public string Bin { get; set; }
