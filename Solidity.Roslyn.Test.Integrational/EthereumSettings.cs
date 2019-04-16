0b2d24678b309d33fed8c22a4d897711ecdff5b1
Psilon <pzixel@gmail.com>
2019-04-16 16:56:12 +0300

    fix

--- Solidity.Roslyn.Test.Integrational/EthereumSettings.cs
+++ Solidity.Roslyn.Test.Integrational/EthereumSettings.cs
@@ -3,7 +3,7 @@
     public static class EthereumSettings
     {
         public const string ParityConnectionString = "http://localhost:8545";
-        public const string AccountAddress = "0x8Ef4fb866d28D87B61229C0740996aD8160A1ecf";
-        public const string AccountPassword = "Maxpass5";
+        public const string AccountAddress = "0x00fb81808899fd51c4a87a642084138be62189a0";
+        public const string AccountPassword = "";
     }
 }
